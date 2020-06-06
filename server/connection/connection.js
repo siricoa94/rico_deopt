eced546d870d18f16059bdb3c8e3e5c0402a9389
unknown <12siricoa94@gmail.com>
2020-06-06 00:46:10 -0400

    updating connection for heroku

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -2,9 +2,7 @@ let mysql = require("mysql");
 
 let connection;
 
-if (process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
+connection = mysql.createconnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
     if (err) throw err;
